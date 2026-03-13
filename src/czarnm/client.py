a72f12c08e4a174f556e19ac18f94da043cd7611
Rafael Coutinho <rafael.coutinho@gmail.com>
2026-03-13 13:35:10 -0300

    Corrige urls

--- src/czarnm/client.py
+++ src/czarnm/client.py
@@ -24,8 +24,8 @@ logger = logging.getLogger(__name__)
 
 # URLs padrão por ambiente
 API_URLS = {    
-    "hml": "https://www.zarcnm-h.cnptia.embrapa.br/zarcnm",
-    "prd": "https://www.zarcnm.cnptia.embrapa.br/zarcnm",
+    "hml": "https://www.zarcnm-h.cnptia.embrapa.br/",
+    "prd": "https://www.zarcnm.cnptia.embrapa.br/",
 }
 
 # Roles exigidos por prefixo de endpoint (ordem: mais específico primeiro)
